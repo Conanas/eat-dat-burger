c2e3cf0151c93ab564378be560327fdc55fd7491
Tom Haigh <tmhaigh@gmail.com>
2020-10-31 13:16:07 +1100

    updated updateOne call inputs

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -21,13 +21,14 @@ router.post("/api/burgers", function(req, res) {
 // update burger
 router.put("/api/burgers/:id", function(req, res) {
     let id = req.params.id;
-    burger.updateOne([{ burger_name: req.body.burger_name, devoured: req.body.devoured }, id, function(err, data) {
+    let condition = `id=${id}`;
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         }
         res.status(200).end();
-    }])
+    })
 })
 
 module.exports = router;
