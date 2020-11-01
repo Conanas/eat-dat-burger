bbd91dfea69b79ebe00375c64ca3c39957feec21
Tom Haigh <tmhaigh@gmail.com>
2020-11-02 10:22:13 +1100

    correct spelling mistake

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Cheeseburger", false),
-("Turkey Bburger", false),
+("Turkey Burger", false),
 ("Goat Burger", false),
 ("Lamb Burger", false),
 ("Chicken Burger", false);
