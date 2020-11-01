2599ef9f6ade8218336354b4a1e4d89ab5c423dd
Tom Haigh <tmhaigh@gmail.com>
2020-11-01 14:43:04 +1100

    added postgres url for database setup

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
 
 // mysql connection configurations
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db"
-});
+let connection;
+if (process.env.DATABASE_URL) {
+    connection = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burger_db"
+    });
+}
 
 // connect to mysql database
 connection.connect(function(err) {
